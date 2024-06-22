67b1d3de1a14c75705e5315d1f3c894fbe5b8cee
“[Akshit <akshitlakhera14@gmail.com>
2024-06-22 11:37:46 +0530

    Cors added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { userRouter } from "./routes/user";
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
+app.use("/*", cors());
+app.route("app/v1/user", userRouter);
+export default app;
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,6 @@
+import { Hono } from "hono";
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
