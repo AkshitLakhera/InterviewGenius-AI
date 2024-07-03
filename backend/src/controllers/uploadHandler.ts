79a85de7339976ba2005bd868aeae92c01561d5f
“[Akshit <akshitlakhera14@gmail.com>
2024-07-03 11:40:31 +0530

    uplaod and midmiddleware controller is ready

--- backend/src/controllers/uploadHandler.ts
+++ backend/src/controllers/uploadHandler.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
+import { uploadCloudianry } from "../utils/cloudinary";
 // handler code
 export const uploadResume = async (req: Request, res: Response) => {
   const file = req.file;
@@ -8,6 +9,11 @@ export const uploadResume = async (req: Request, res: Response) => {
     return res.status(400).json({ Error: "File not found " });
   }
   // Save  the uploaded file tempororarly
-  const filePath = path.join(__dirname, "..", "uploads", file.originalname);
-  await fs.promises.writeFile(filePath, file.buffer);
+  const filePath = path.join("/public", "tmp", file.originalname);
+  // upload  file to cloudinary
+  const cloudinaryResponse = await uploadCloudianry(filePath);
+  if (!cloudinaryResponse) {
+    return res.status(500).json({ error: "failed  to upload to cloudianry" });
+  }
+  // call python script to process the file
 };
